3f6d2a90cafa6fc64159540c36000f25283d0a72
Ad-White <chalkalino@gmail.com>
2023-07-16 19:07:25 +0000

    Adds the tileGenerator function.

--- script.js
+++ script.js
@@ -8,6 +8,64 @@ function playGame() {
     playBtn.remove();
     welcome.remove();
     createGameArea();
+    
+    let gameLevel = 1;
+    let generatorStarted = true;
+
+    let attempts = 7;
+
+    let startBtn = document.getElementById('startBtn');
+    startBtn.addEventListener('click', startGenerator);
+
+    let stopBtn = document.getElementById('stopBtn');
+    stopBtn.addEventListener('click', stopGenerator);
+
+    let quitBtn = document.getElementById('quitBtn');
+    quitBtn.addEventListener('click', quitGame);
+
+    messageArea.innerHTML = `<p>Press Start To Begin Game</p>`;
+    
+    /** The startGenerator function checks conditions are true for variables,
+     *  generatorStarted, attempts and gameLevel.
+     *  If true, the generator starts by calling the tileGenerator function.
+     *  If conditions are not met, the generatorStarted variable is set 
+     *  to false and the generator is in a stopped state.
+     */
+    function startGenerator() {
+
+        if (generatorStarted === true && attempts > 0 && gameLevel <= 3) {
+          messageArea.innerHTML = `<p>Press Stop To Select Your Tile</p>`;
+    
+          tileGenerator();
+    
+        } else {
+          generatorStarted = false;
+    
+          console.log('generator stopped');
+          reveal();
+          askForGuess();
+        }
+      }
+    
+
+      /** The tileGenerator function returns all elements with classes of .tile and .inPlay as an HTML Collection.
+       *  It chooses one of these elements at random. The randomTile is passed to the addHighLight function
+       *  where it applies a highlight to the random tile. 
+       *  The variable name is then changed to selectedTile, where it then gets used in the function, stopGenerator.
+       */
+      function tileGenerator() {
+
+        // eventListener is removed to prevent multiple starting of the generator
+        startBtn.removeEventListener('click', startGenerator);
+    
+        let tiles = document.querySelectorAll(".tile" && ".inPlay");
+    
+        let randomTile = tiles[Math.floor(Math.random() * tiles.length)];
+        addHighLight(randomTile);
+        selectedTile = randomTile;
+    
+        console.log(randomTile);
+      }
 
 
     /** This function creates the gameboard table.
@@ -40,27 +98,26 @@ function playGame() {
         <td id="11" class="tile inPlay">?</td>
         <td id="12" class="tile inPlay">?</td>
         </tr>
-    </table>
-    <div id="messageArea"></div>
-    <div id="userAnswer"></div>
-    
-    <div id="buttons">
+        </table>
+        <div id="messageArea"></div>
+        <div id="userAnswer"></div>
         
-    <button id="startBtn" class="button">Start</button>
-    <button id="stopBtn" class="button">Stop</button>
-
-    </div>
+        <div id="buttons">
             
-    <div id="currentLevel">
-        current level
-    </div>
-    <div id="attemptsRemaining">
-        attempts remaining
-    </div>
+        <button id="startBtn" class="button">Start</button>
+        <button id="stopBtn" class="button">Stop</button>
 
-    <div id="quit">
-        <button id="quitBtn" class="button">Quit</button>
-    </div>
-    `
+        </div>
+                
+        <div id="currentLevel">
+            current level
+        </div>
+        <div id="attemptsRemaining">
+            attempts remaining
+        </div>
+
+        <div id="quit">
+            <button id="quitBtn" class="button">Quit</button>
+        </div>`
     }
 }
